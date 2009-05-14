bb84b9905aaccc5b3d72d814a9ccb15895c91457
Victor <vchipnt-dev@yahoo.com.br>
2009-05-14 00:27:24 -0300

    new things in script

--- 01/codigo/mk_tree.sh
+++ 01/codigo/mk_tree.sh
@@ -7,18 +7,32 @@ OBJ_DIR=$(grep "^ODIR" Makefile | cut -f 2 -d "=");
 # OUT_DIR is in src/main.c
 OUT_DIR=$(grep "#define GNUPLOT_DIR" src/main.c | sed -ne 's/.*"\(.*\)".*/\1/p');
 
+# FILE is in src/main.c
+FILE=$(grep "#define .*SUBGRAD" src/main.c | sed -ne 's/.*"\(.*\)".*/\1/p');
+
+
 echo ">>> Making dirs";
 
 for i in {$OBJ_DIR,$OUT_DIR}
 do
     if [[ -e $i ]]
     then
-	echo ">> $i is ready :P";
+	echo ">>> $i is ready :P";
     else
-	echo ">> Creating $i";
+	echo ">>> Creating $i";
 	mkdir $i;
     fi
 done;
+
+if [[ -e $FILE ]]
+then
+    echo ">>> $FILE is ready :P";
+else
+    echo ">>> Creating Param file";
+    echo "e = 2" > $FILE;
+    echo "max_iter = 600" >> $FILE;
+    echo "max_iter_no_improv = 10" >> $FILE;
+fi;
 
 echo ">>> End";
 
